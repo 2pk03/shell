4d87a292a188ca48dccaa8b83a369e0300cd1032
Alexander Alten-Lorenz <wget.null@gmail.com>
2016-11-30 09:48:14 +0100

    Update rmhdp.bash

--- rmhdp.bash
+++ rmhdp.bash
@@ -24,7 +24,7 @@ echo "=> remove files and directories"
 rm -rf /etc/hadoop /etc/falcon /etc/hive /etc/hbase /etc/oozie /etc/sqoop /etc/hue /etc/zookeeper /etc/flume /etc/storm* /etc/hive* \
 /etc/tez /etc/knox /etc/kafka /etc/slider /etc/spark /etc/pig /etc/phoenix /etc/ranger* /etc/zeppelin /etc/atlas \
 /var/run/hadoop* /var/run/hbase /var/run/hive* /var/run/oozie /var/run/sqoop /var/run/hue /var/run/zookeeper /var/run/flume \
-/var/run/storm /var/run/falcon /var/run/webhcat /var/run/knox /var/run/kafka /var/run/spark /var/run/ranger /var/run/ambari* \ 
+/var/run/storm /var/run/falcon /var/run/webhcat /var/run/knox /var/run/kafka /var/run/spark /var/run/ranger /var/run/ambari* \
 /var/run/atlas /var/log/hbase /var/log/hive* /var/log/oozie /var/log/sqoop /var/log/hue /var/log/zookeeper /var/log/flume \
 /var/log/storm /var/log/falcon /var/log/webhcat /var/log/hadoop* /var/log/knox /var/log/kafka /var/log/spark /var/log/ranger \
 /var/log/ambari* /var/log/atlas /var/log/zeppelin /usr/lib/flume /usr/lib/storm /var/lib/atlas /var/tmp/oozie /tmp/hive* \
